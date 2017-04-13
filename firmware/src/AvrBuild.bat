7893cdc5a402b35df9fc90f55666715adf05f763
ps <ps@ps-pc.lan>
2017-04-13 19:57:16 +0300

    add flash.sh

--- firmware/src/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\project\sg-420m\firmware\src\labels.tmp" -fI -W+ie -C V2E -o "C:\project\sg-420m\firmware\src\SG-420M.hex" -d "C:\project\sg-420m\firmware\src\sg-420m.obj" -e "C:\project\sg-420m\firmware\src\SG-420M.eep" -m "C:\project\sg-420m\firmware\src\SG-420M.map" "C:\project\sg-420m\firmware\src\Main.asm"
--- firmware/src/flash.bat
+++
@@ -1,8 +0,0 @@
-SET avrtooldir=c:\progra~2\atmel\avrtoo~1
-SET avrflashdir=%avrtooldir%\stk500
-
-SET target=SG-420M
-
-%avrflashdir%\stk500.exe -dATmega8 -ms -e -fC92F -EFF -FC92F -GFF -pf -vf -if%target%.hex -lFC -LFC
-
-PAUSE
